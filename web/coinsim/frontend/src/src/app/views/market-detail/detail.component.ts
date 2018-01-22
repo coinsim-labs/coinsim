53214430a2045f15dee33f93b16aff877e00cd12
Felix Cornelius <mail@felixcornelius.de>
2018-01-22 15:09:48 +0100

    Detail component placeholder text

--- web/coinsim/frontend/src/src/app/views/market-detail/detail.component.ts
+++ web/coinsim/frontend/src/src/app/views/market-detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { CoinsimService } from '../../coinsim.service'
+import { CryptoCompareService } from '../../cryptocompare.service'
 @Component({
   templateUrl: 'detail.component.html',
   styleUrls: ['detail.component.css']
@@ -9,12 +10,16 @@ import { CoinsimService } from '../../coinsim.service'
 export class DetailComponent implements OnInit {
   currency: string;
   currency_map: any;
+  price: any;
 
-  constructor(private cs: CoinsimService, private route: ActivatedRoute) {
+  constructor(private cs: CoinsimService, private ccs: CryptoCompareService, private route: ActivatedRoute) {
     this.route.params.subscribe(
         params => {
           this.currency = params['currency']
-
+          this.ccs.multiCryptoPrice(this.currency, 'USD', null, null, null, null, true).subscribe(price => {
+            this.price = price['DISPLAY'][this.currency]['USD'];
+            console.log(this.price);
+          })
         }
     ); 
   }
@@ -23,6 +28,8 @@ export class DetailComponent implements OnInit {
     this.cs.currencyMap().subscribe(currencies => {
       this.currency_map = currencies
     })
+
+    
   }
 
 }
