0925880963aa0cbd0aaf7ad86438b395f699e0fa
Tim Schmittmann <tim.schmittmann@gmx.de>
2018-01-23 03:51:53 +0100

    Add cryptoDescription to detail-view

--- web/coinsim/frontend/src/src/app/views/market-detail/detail.component.ts
+++ web/coinsim/frontend/src/src/app/views/market-detail/detail.component.ts
@@ -3,14 +3,17 @@ import {ActivatedRoute} from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { CoinsimService } from '../../coinsim.service'
 import { CryptoCompareService } from '../../cryptocompare.service'
+import {Observable} from "rxjs/Observable";
+
 @Component({
   templateUrl: 'detail.component.html',
-  styleUrls: ['detail.component.css']
+  styleUrls: ['detail.component.scss']
 })
 export class DetailComponent implements OnInit {
   currency: string;
   currency_map: any;
   price: any;
+  cryptoDescription: Observable<Object[]>
 
   constructor(private cs: CoinsimService, private ccs: CryptoCompareService, private route: ActivatedRoute) {
     this.route.params.subscribe(
@@ -21,16 +24,30 @@ export class DetailComponent implements OnInit {
             console.log(this.price);
           })
         }
-    ); 
+    );
   }
 
   ngOnInit() {
     this.cs.refresh();
     this.cs.currencyMap().subscribe(currencies => {
-      this.currency_map = currencies
+      this.currency_map = currencies;
+      // let page = this.coingecko.parseCryptoDescription(this.currency_map['BTC'].name).subscribe(result => {
+      //   console.log(result);
+      //   debugger;
+      // });
     })
+    this.cryptoDescription = this.getCryptoDescription();
+  }
 
-    
+  getCryptoDescription() {
+    return this.cs.cryptoDescriptions().map(
+      descriptions => {
+        let filtered = descriptions.filter(
+          description => {
+            return description.currency === this.currency;
+          });
+        return filtered[0].text;
+      });
   }
 
 }
